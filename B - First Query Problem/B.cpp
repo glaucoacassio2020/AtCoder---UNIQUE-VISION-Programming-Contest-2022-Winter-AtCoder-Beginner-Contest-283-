78c8670bf46d3bcb81d6b039234e3230a9199b33
glaucogithub <75082173+glaucogithub@users.noreply.github.com>
2023-02-04 22:26:01 -0400

    Upload Atcoder

---
+++ B - First Query Problem/B.cpp
@@ -0,0 +1,89 @@
+
+// author: glaucoacassioc
+// created on: 04/02/2023 às 20:40:57
+
+// Problem: B - First Query Problem
+// URL: https://atcoder.jp/contests/abc283/tasks/abc283_b
+// Time Limit: 2000 ms
+// Memory Limit: 1024 MB 
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+ 
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+	Fun fun_;
+public:
+	template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+	template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+ 
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template < typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream & operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef DEBUG
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+ 
+ 
+void run_case() {
+	int n;
+	cin >> n;
+	vector<int> A(n);
+	for(auto &a : A) {
+		cin >> a;
+	}
+	int q;
+	cin >> q;
+	while(q-- > 0) {
+		int op;
+		cin >> op;
+		if(op == 1) {
+			// update
+			int k, x;
+			cin >> k >> x;
+			A[k - 1] = x;
+		} else {
+			// query
+			int k;
+			cin >> k;
+			cout << A[k - 1] << '\n';
+		}
+	}
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+#ifdef DEBUG
+	// freopen("sample1.in", "r", stdin);
+#endif
+	int T = 1;
+	// cin >> T;
+	while (T-- > 0) {
+		run_case();
+	}
+}
+ 
+ 
